52a77d5470daa7aae735523b0fd8c5043430fa7b
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2020-11-08 20:24:31 -0600

    An attempt at syntax highlighting with highlight.js

---
+++ syntax-highlighting/calypso.js
@@ -0,0 +1,58 @@
+// highlight.js syntax highlighting for Calypso
+
+const calypso = (hljs) => {
+  let KEYWORDS = 'is if else for in loop while match ' +
+                 'ret break fn native mod use import pub ' +
+                 'let mut del as';
+
+  let BUILTINS = "println";
+    
+  return {
+    name: 'Calypso',
+    aliases: ['cal'],
+    keywords: {
+      $pattern: hljs.IDENT_RE + '!?',
+      keyword: KEYWORDS,
+      literal: 'true false null undef',
+      built_in: BUILTINS
+    },
+    illegal: "</",
+    contains: [
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.COMMENT('/\\*', '\\*/', {contains: ['self']}),
+      hljs.QUOTE_STRING_MODE,
+      {
+        className: 'string',
+        begin: /'\\?(x\w{2}|u\w{4}|U\w{8}|.)'/
+      },
+      {
+        className: 'number',
+        variants: [
+          { begin: '\\b0b([01_]+)' },
+          { begin: '\\b0o([0-7_]+)' },
+          { begin: '\\b0x([A-Fa-f0-9_]+)' },
+          { begin: '\\b(\\d[\\d_]*(\\.[0-9_]+)?([eE][+-]?[0-9_]+)?)'}
+        ],
+        relevance: 0
+      },
+      {
+        className: 'function',
+        beginKeywords: 'fn', end: '\\(', excludeEnd: true,
+        contains: [hljs.UNDERSCORE_TITLE_MODE]
+      },
+      {
+        className: 'meta',
+        begin: '#!?\\[', end: '\\]',
+        contains: [
+          {
+            className: 'meta-string',
+            begin: /"/, end: /"/
+          }
+        ]
+      }
+    ]
+  }
+};
+
+hljs.registerLanguage("calypso", calypso);
+hljs.registerAliases("cal", {languageName: "calypso"});
