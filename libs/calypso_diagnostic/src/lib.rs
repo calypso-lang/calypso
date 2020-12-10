46991ae56e66fccd63c902c9583cf21a27681f07
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2020-12-09 22:42:06 -0600

    expect(fail)+wip: GlobalReportingCtxt + cleanup gen_error! macro

--- libs/calypso_diagnostic/src/lib.rs
+++ libs/calypso_diagnostic/src/lib.rs
@@ -9,7 +9,6 @@ extern crate lazy_static;
 pub mod diagnostic;
 pub mod error;
 pub mod report;
-pub mod sync;
 pub mod types;
 
 pub extern crate codespan_reporting as reporting;
@@ -20,29 +19,26 @@ pub extern crate strfmt;
 #[macro_export]
 macro_rules! gen_error {
     (Err($($rest:tt)*) as $ty:ty) => {
-        $crate::error::Result::<$ty>::Err($crate::real_gen_error!($($rest)*).into())
+        $crate::error::Result::<$ty>::Err($crate::gen_error!(@i1 $($rest)*).into())
     };
 
     ($report:ident, $($rest:tt)*) => {{
-        $report.add($crate::real_gen_error!($($rest)*));
+        $report.add($crate::gen_error!(@i1 $($rest)*));
     }};
 
     ($report:expr, $($rest:tt)*) => {{
         let mut report = $report;
-        report.add($crate::real_gen_error!($($rest)*));
+        report.add($crate::gen_error!(@i1 $($rest)*));
         report
-    }}
-}
-
-#[macro_export]
-macro_rules! real_gen_error {
-
-    (@labels $diagnostic:ident, [$($style:expr => ($source:expr, $span:expr); $message:expr),*$(,)?]) => {{
-        $diagnostic = $diagnostic$(.label($style, $message, $span, $source))*;
     }};
 
-    (@notes $diagnostic:ident, [$($note:expr),*$(,)?]) => {{
+    (@i3 $diagnostic:ident; notes: [$($note:expr),*$(,)?]) => {{
        $diagnostic = $diagnostic$(.note($note))*;
+    }};
+
+    (@i2 $diagnostic:ident; labels: [$($style:expr => ($source:expr, $span:expr); $message:expr),*$(,)?]$(, $($rest:tt)*)?) => {{
+        $diagnostic = $diagnostic$(.label($style, $message, $span, $source))*;
+        $($crate::gen_error!(@i3 $diagnostic; $($rest)*))?;
     }};
 
     (@fmt $diag:expr, $($name:ident = $value:expr),*) => {{
@@ -55,7 +51,7 @@ macro_rules! real_gen_error {
         $diag
     }};
 
-    ($self:expr => { $code:ident$(, $($name:ident = $value:expr),*)?; labels: $labels:tt, notes: $notes:tt$(,)? }) => {{
+    (@i1 $self:expr => { $code:ident$(, $($name:ident = $value:expr),*)?; $($rest:tt)* }) => {{
         let mut diagnostic = $crate::diagnostic::DiagnosticBuilder::new(
             $crate::diagnostic::Severity::Error,
             &($self).files,
@@ -63,72 +59,14 @@ macro_rules! real_gen_error {
         let code = stringify!($code);
         let diag = $crate::types::DIAGNOSTICS.get(code).unwrap();
         diagnostic = diagnostic.code(code)
-            .message($crate::real_gen_error!(@fmt diag.0$($(,$name = $value)*)?));
-
-        $crate::real_gen_error!(@labels diagnostic, $labels);
-        $crate::real_gen_error!(@notes diagnostic, $notes);
-
+            .message($crate::gen_error!(@fmt diag.0$($(,$name = $value)*)?));
         if diag.1.is_some() {
             diagnostic = diagnostic.note(format!("note: this error has more details for troubleshooting, run `calypso explain {}`", code))
         };
 
+        $crate::gen_error!(@i2 diagnostic; $($rest)*);
         diagnostic.build()?
     }};
-
-    ($self:expr => { $code:ident$(, $($name:ident = $value:expr),*)?; labels: $labels:tt$(,)?}) => {{
-        let mut diagnostic = $crate::diagnostic::DiagnosticBuilder::new(
-            $crate::diagnostic::Severity::Error,
-            &($self).files,
-        );
-        let code = stringify!($code);
-        let diag = $crate::types::DIAGNOSTICS.get(code).unwrap();
-        diagnostic = diagnostic.code(code)
-            .message($crate::real_gen_error!(@fmt diag.0$($(,$name = $value)*)?));
-
-        $crate::real_gen_error!(@labels diagnostic, $labels);
-
-        if diag.1.is_some() {
-            diagnostic = diagnostic.note(format!("note: this error has more details for troubleshooting, run `calypso explain {}`", code))
-        };
-
-        diagnostic.build()?
-    }};
-
-    ($self:expr => { $code:ident$(, $($name:ident = $value:expr),*)?; notes: $notes:tt$(,)? }) => {{
-        let mut diagnostic = $crate::diagnostic::DiagnosticBuilder::new(
-            $crate::diagnostic::Severity::Error,
-            &($self).files,
-        );
-        let code = stringify!($code);
-        let diag = $crate::types::DIAGNOSTICS.get(code).unwrap();
-        diagnostic = diagnostic.code(code)
-            .message($crate::real_gen_error!(@fmt diag.0$($(,$name = $value)*)?));
-
-        $crate::real_gen_error!(@notes diagnostic, $notes);
-
-        if diag.1.is_some() {
-            diagnostic = diagnostic.note(format!("note: this error has more details for troubleshooting, run `calypso explain {}`", code))
-        };
-
-        diagnostic.build()?
-    }};
-
-    ($self:expr => { $code:ident$(, $($name:ident = $value:expr),*)?$(;)? }) => {{
-        let mut diagnostic = $crate::diagnostic::DiagnosticBuilder::new(
-            $crate::diagnostic::Severity::Error,
-            &($self).files,
-        );
-        let code = stringify!($code);
-        let diag = $crate::types::DIAGNOSTICS.get(code).unwrap();
-        diagnostic = diagnostic.code(code)
-            .message($crate::real_gen_error!(@fmt diag.0$($(,$name = $value)*)?));
-
-        if diag.1.is_some() {
-            diagnostic = diagnostic.note(format!("note: this error has more details for troubleshooting, run `calypso explain {}`", code))
-        };
-
-        diagnostic.build()?
-    }}
 }
 
 pub mod prelude {
@@ -138,7 +76,6 @@ pub mod prelude {
         ResultExt as CalResultExt,
     };
     pub use super::gen_error;
-    pub use super::sync::prelude::*;
     pub use super::FileMgr;
     pub use error_chain::ChainedError;
 }
--- libs/calypso_diagnostic/src/report.rs
+++ libs/calypso_diagnostic/src/report.rs
@@ -1,52 +1,39 @@
-use super::diagnostic::Diagnostic;
-
-use std::ops::{Deref, DerefMut};
+use crate::diagnostic::Diagnostic;
 
 #[derive(Clone, Debug, Default)]
-/// An ordered group of diagnostics. This is basically
-/// currently just a wrapper over `Vec<Diagnostic>`.
-pub struct Report {
-    diagnostics: Vec<Diagnostic>,
+pub struct GlobalReportingCtxt {
+    errors: Vec<SyncState>,
+    nonfatals: Vec<Diagnostic>,
+    panic: bool,
 }
 
-impl Report {
+impl GlobalReportingCtxt {
     pub fn new() -> Self {
-        Self { ..Self::default() }
+        Self::default()
     }
 
-    pub fn add(&mut self, diagnostic: Diagnostic) -> &mut Self {
-        self.diagnostics.push(diagnostic);
-        self
+    pub fn report_fatal(&mut self, value: Diagnostic) {
+        self.errors.push(Err(value));
+    }
+
+    pub fn report_syncd(&mut self, value: Diagnostic) {
+        self.errors.push(Ok(value));
+    }
+
+    pub fn report_non_fatal(&mut self, value: Diagnostic) {
+        self.nonfatals.push(value);
+    }
+
+    pub fn nonfatals(&self) -> &[Diagnostic] {
+        &self.nonfatals
+    }
+
+    pub fn errors(&self) -> &[SyncState] {
+        &self.errors
     }
 }
 
-impl AsRef<[Diagnostic]> for Report {
-    fn as_ref(&self) -> &[Diagnostic] {
-        &self.diagnostics
-    }
-}
-
-impl Deref for Report {
-    type Target = Vec<Diagnostic>;
-    fn deref(&self) -> &Self::Target {
-        &self.diagnostics
-    }
-}
-
-impl DerefMut for Report {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.diagnostics
-    }
-}
-
-impl From<Report> for Vec<Diagnostic> {
-    fn from(report: Report) -> Self {
-        report.diagnostics
-    }
-}
-
-impl From<Vec<Diagnostic>> for Report {
-    fn from(diagnostics: Vec<Diagnostic>) -> Self {
-        Self { diagnostics }
-    }
-}
+/// A synchronization state. `Ok(Diagnostic)` means
+/// synchronization was required and `Err(Diagnostic)`
+/// means that it was a fatal error.
+type SyncState = Result<Diagnostic, Diagnostic>;
