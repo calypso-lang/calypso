b47828177fd01e850ba8b7bd70b05aebfbaa6038
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2021-01-18 22:14:21 -0600

    Index page for books

--- docs/build-books.sh
+++ docs/build-books.sh
@@ -4,6 +4,7 @@
 # |- ./out/gui/
 # |- ./out/ref/
 # |- ./out/rustdoc/
+# Also copies static/* to ./out/
 # This assumes you already have mdbook, cargo, and nightly rust installed, and are in /docs, relative to the repository root.
 set -o xtrace
 set -Eeuxo pipefail
@@ -34,5 +35,9 @@ echo "Moving files to the out directory"
 mv -f ./guide/book ./out/gui || exit 1
 mv -f ./reference/book ./out/ref || exit 1
 mv -f ../target/doc ./out/rustdoc || exit 1
+
+echo "Copying static files to the out directory"
+
+cp -rf ./static/* ./out/ || exit 1
 
 echo "Success!"
