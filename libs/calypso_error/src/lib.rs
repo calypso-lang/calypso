605a9e6666aca56dada610b7f6fe168ab4079c2e
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2021-02-21 15:58:15 -0600

    docs docs docs

--- libs/calypso_error/src/lib.rs
+++ libs/calypso_error/src/lib.rs
@@ -1,3 +1,5 @@
+//! Error-handling utilities and types for use in
+//! [Calypso](https://github.com/calypso-lang/calypso).
 #![doc(html_root_url = "https://calypso-lang.github.io/rustdoc/calypso_error/index.html")]
 #![warn(clippy::pedantic)]
 
@@ -6,10 +8,13 @@ use thiserror::Error;
 /// The Calypso error type.
 #[derive(Error, Debug)]
 pub enum CalError {
+    /// IO errors
     #[error("i/o error")]
     Io(#[from] std::io::Error),
+    /// UTF-8 decoding errors
     #[error("utf-8 decoding error")]
     FromUtf8(#[from] std::string::FromUtf8Error),
+    /// Any other error, using [`anyhow`]
     #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
