66c9dc616cc4792360342bce3dc062a85f95754f
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2022-02-27 01:23:25 -0600

    Remove old stuff from SaturnVM

--- libs/saturnvm/src/lib.rs
+++ libs/saturnvm/src/lib.rs
@@ -1,9 +1,2 @@
 #![doc(html_root_url = "https://calypso-lang.github.io/rustdoc/saturnvm/index.html")]
 #![warn(clippy::pedantic)]
-
-/*
-planning
-
-struct RegisterBlock =
-    integrals: [uint; 255]
-*/
--- libs/saturnvm_builder/src/lib.rs
+++ libs/saturnvm_builder/src/lib.rs
@@ -1,4 +1,2 @@
 #![doc(html_root_url = "https://calypso-lang.github.io/rustdoc/saturnvm_builder/index.html")]
 #![warn(clippy::pedantic)]
-
-pub mod ir;
