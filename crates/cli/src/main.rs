e09d73b9a641c06adc51bf9156d88d354f8c7f3f
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2026-05-17 10:50:53 -0500

    Skeleton

---
+++ crates/cli/src/main.rs
@@ -0,0 +1,4 @@
+#![warn(clippy::pedantic)]
+#![allow(clippy::must_use_candidate, clippy::return_self_not_must_use)]
+
+fn main() {}
---
+++ crates/compiler/src/lib.rs
@@ -0,0 +1,2 @@
+#![warn(clippy::pedantic)]
+#![allow(clippy::must_use_candidate, clippy::return_self_not_must_use)]
